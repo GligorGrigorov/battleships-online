bd55042bda948ed87d3d585e4e9eef69eb4dc988
Gligor <grigorovgligor@gmail.com>
2021-02-13 11:02:32 +0200

    Remove unnecessary code

--- src/bg/uni/sofia/fmi/mjt/battleships/game/Status.java
+++ src/bg/uni/sofia/fmi/mjt/battleships/game/Status.java
@@ -4,21 +4,10 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public enum Status implements Serializable {
-    PENDING("pending"),
-    IN_PROGRESS("in progress"),
-    FINISHED("finished");
+    PENDING,
+    IN_PROGRESS,
+    FINISHED;
 
     @Serial
     private static final long serialVersionUID = 2669480397026438792L;
-
-    private final String status;
-
-    Status(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return status;
-    }
 }
